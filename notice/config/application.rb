2c53a2d11376812c1e84c4cca12d9fb86b8e1d9d
odajunpei <i.penju1022@gmail.com>
2021-10-15 05:22:39 +0000

    [fix]timezone設定

--- notice/config/application.rb
+++ notice/config/application.rb
@@ -12,5 +12,7 @@ module Notice
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ja #日本語化
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
