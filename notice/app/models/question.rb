595e668aa0be18674751e7cf672a3979658fab6d
odajunpei <i.penju1022@gmail.com>
2021-11-05 07:29:34 +0000

    aaa

--- notice/app/models/question.rb
+++ notice/app/models/question.rb
@@ -1,4 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :member
   belongs_to :user
+
+  validates :body, presence: true
+  validates :choices1, length: { minimum: 1, maximum: 10 }
+  validates :choices2, length: { minimum: 1, maximum: 10 }
 end
