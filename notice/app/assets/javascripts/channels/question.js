9a99a4cdf70efabf6abc38fb7b0c6d2e25710815
odajunpei <i.penju1022@gmail.com>
2021-11-06 09:29:26 +0000

    fix

--- notice/app/assets/javascripts/channels/question.js
+++ notice/app/assets/javascripts/channels/question.js
@@ -9,7 +9,13 @@
 
    received: function(data) {
      // Called when there's incoming data on the websocket for this channel
-      const html = `<p>${data.content}</p>`;
+      const html = `<p>${data.content.body}</p>
+                    <p>${data.content.member.nickname}</p>
+                    <p>${data.content.member.profile_image}</p>
+                    <p>${data.content.created_at.strftime('%Y/%m/%d %H:%M:%S')}</p>
+                    <p>${data.content.choices1}</p>
+                    <p>${data.content.choices2}</p>
+                    <p>${data.content.choices3}</p>`;
       const messages = document.getElementById('messages');
     //   const newMessage = document.getElementById('message_text');
       messages.insertAdjacentHTML('afterbegin', html);
