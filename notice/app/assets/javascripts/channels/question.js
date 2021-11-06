12b211e559a6628cbcfb2e94ff512a7bd84e4db7
odajunpei <i.penju1022@gmail.com>
2021-11-06 06:32:06 +0000

    fixed question.js

--- notice/app/assets/javascripts/channels/question.js
+++ notice/app/assets/javascripts/channels/question.js
@@ -1,19 +1,24 @@
-// App.question = App.cable.subscriptions.create("QuestionChannel", {
-//   connected: function() {
-//     // Called when the subscription is ready for use on the server
-//   },
+ App.question = App.cable.subscriptions.create("QuestionChannel", {
+   connected: function() {
+     // Called when the subscription is ready for use on the server
+   },
 
-//   disconnected: function() {
-//     // Called when the subscription has been terminated by the server
-//   },
+   disconnected: function() {
+     // Called when the subscription has been terminated by the server
+   },
 
-//   received: function(data) {
-//     // Called when there's incoming data on the websocket for this channel
-//   }
-// });
+   received: function(data) {
+     // Called when there's incoming data on the websocket for this channel
+      const html = `<p>${data.content.text}</p>`;
+      const messages = document.getElementById('questions');
+      const newMessage = document.getElementById('question_text');
+      messages.insertAdjacentHTML('afterbegin', html);
+      newMessage.value='';
+   }
+ });
 
-import consumer from "./consumer"
-
+// import consumer from "./consumer"
+/*
 consumer.subscriptions.create("QuestionChannel", {
   received(data) {
     // 以下で使用
@@ -24,3 +29,4 @@ consumer.subscriptions.create("QuestionChannel", {
     newMessage.value='';
   }
 });
+*/
