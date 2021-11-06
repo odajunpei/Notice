b4a30e5166a040bf448ce3f2a7d61195761245ce
odajunpei <i.penju1022@gmail.com>
2021-11-06 08:59:08 +0000

    fix

--- notice/app/assets/javascripts/channels/question.js
+++ notice/app/assets/javascripts/channels/question.js
@@ -11,22 +11,8 @@
      // Called when there's incoming data on the websocket for this channel
       const html = `<p>${data.content.text}</p>`;
       const messages = document.getElementById('messages');
-      // const newMessage = document.getElementById('question_text');
+      const newMessage = document.getElementById('message_text');
       messages.insertAdjacentHTML('afterbegin', html);
-      // newMessage.value='';
+      newMessage.value='';
    }
  });
-
-// import consumer from "./consumer"
-/*
-consumer.subscriptions.create("QuestionChannel", {
-  received(data) {
-    // 以下で使用
-    const html = `<p>${data.content.text}</p>`;
-    const messages = document.getElementById('questions');
-    const newMessage = document.getElementById('question_text');
-    messages.insertAdjacentHTML('afterbegin', html);
-    newMessage.value='';
-  }
-});
-*/
