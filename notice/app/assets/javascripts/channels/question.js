cb46d2ddb27b00db3d2f3ab22769e2b952c65ef4
odajunpei <i.penju1022@gmail.com>
2021-11-06 10:26:26 +0000

    fix

--- notice/app/assets/javascripts/channels/question.js
+++ notice/app/assets/javascripts/channels/question.js
@@ -11,7 +11,12 @@
      // Called when there's incoming data on the websocket for this channel
      console.log(data);
       const html = `<p>${data.content.body}</p>
-                    <p>${data.content.nickname}</p>`;
+                    <p>${data.content.nickname}</p>;
+                    // <p>${data.content.profile_image}</p>
+                    // <p>${data.content.created_at}</p>
+                    // <p>${data.content.choices1}</p>
+                    // <p>${data.content.choices2}</p>
+                    // <p>${data.content.choices3}</p>`;
       const messages = document.getElementById('messages');
     //   const newMessage = document.getElementById('message_text');
       messages.insertAdjacentHTML('afterbegin', html);
