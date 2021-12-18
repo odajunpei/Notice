69f5d361465947f754bc49b3c807fa6fefd0c766
odajunpei <i.penju1022@gmail.com>
2021-12-18 09:48:16 +0000

    fixcount

--- notice/app/controllers/homes_controller.rb
+++ notice/app/controllers/homes_controller.rb
@@ -1,5 +1,9 @@
 class HomesController < ApplicationController
   def top
+    @see = See.find_by(ip: request.remote_ip)
+    unless @see
+      See.create(ip: request.remote_ip)
+    end
   end
 
   def inquiry
